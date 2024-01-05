c062f4ed0e8f476b5ec6af658fdad84848f175f0
AUTOMATIC <->
2024-01-05 11:36:38 +0300

    update for wizardcoder

--- utils.py
+++ utils.py
@@ -21,19 +21,31 @@ prompt_templates: list[PromptTemplate] = []
 
 
 def load_templates(dirname):
-    s = []
+    templates = {}
 
     for filename in os.listdir(dirname):
         with open(dirname + "/" + filename, "r", encoding="utf8") as file:
             jsdata = json.load(file)
 
-        template = PromptTemplate(**jsdata)
-        s.append(template)
+        templates[jsdata["name"]] = jsdata
 
-    s = sorted(s, key=lambda x: x.priority, reverse=True)
+    res = []
+
+    for jsdata in templates.values():
+        copyfrom = jsdata.pop('copyfrom', None)
+        if copyfrom is not None:
+            src = templates[copyfrom]
+            for k, v in src.items():
+                if k not in jsdata:
+                    jsdata[k] = v
+
+        template = PromptTemplate(**jsdata)
+        res.append(template)
+
+    res = sorted(res, key=lambda x: x.priority, reverse=True)
 
     prompt_templates.clear()
-    prompt_templates.extend(s)
+    prompt_templates.extend(res)
 
 
 def find_prompt_template(modelname):
